352ff3fe2cae3b0e14847e80aadd28cee2b747b9
samreshperchani@hotmail.com <samreshperchani@hotmail.com>
2019-10-10 15:02:08 +0200

    added configuration files

---
+++ config/config.py
@@ -0,0 +1,49 @@
+
+# path to base directory
+BASE_DIR = 'E:/University/Thesis/submission/dbkwik_instance_matching_using_embeddings/'
+
+# directory where data is present
+DATA_DIR = 'data'
+
+# directory where processed xml dumps are present
+DATA_DUMPS_DIR = 'data_dumps'
+
+# directory where processed dumps are present
+PROCESSED_DUMPS_DIR = 'processed_dumps'
+
+# directory where untared xml dumps are present
+UNTARED_DUMPS_DIR = 'extracted_dumps'
+
+# directory where extracted text will be stored
+EXTRACTED_TEXT_DIR = 'extracted_text'
+
+# directory where processed dumps are present
+EXTRACTED_PROCESSED_DUMPS_DIR = 'extracted_processed_dumps'
+
+# directory where doc2vec model and code is present
+DOC2VEC_DIR = 'doc2vec'
+
+# directory where rdf2vec model and code is present
+RDF2VEC_DIR = 'rdf2vec'
+
+# directory where word2vec model and code is present
+WORD2VEC_DIR = 'word2vec'
+
+# path to stats directory
+STATS_DIR = 'stats_parts'
+
+# path to labels directory
+LABELS_DIR = 'labels_full_en_ds/labels/'
+
+# gold standard directory
+GS_DIR = 'gold_standard'
+
+# gold standard entities labels
+GS_ENTITIES_LABELS = 'gold_standard_entities_labels'
+
+
+#correspondances directory
+CORRESPONDANCES_DIR = 'correspondances'
+
+# gold standard labels file
+GS_ENTITIES_LABELS_FILE = 'gs_entities_labels_wo_lyrics.pkl'
